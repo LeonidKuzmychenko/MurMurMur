e9cdf7915b9ac77fe4ce9a5f0a9584a368d584b3
Kuzmychenko <leonid.kuzmychenko@ab-soft.net>
2020-09-06 23:02:50 +0300

    icons

--- app/src/main/java/com/example/murmurmur/StartActivity.kt
+++ app/src/main/java/com/example/murmurmur/StartActivity.kt
@@ -4,13 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
-        startActivity(Intent(applicationContext, MainActivity::class.java))
+        GlobalScope.launch {
+            Thread.sleep(5000)
+            startActivity(Intent(this@StartActivity, MainActivity::class.java))
+            finish()
+        }
 
     }
 
